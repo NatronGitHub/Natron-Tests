cf0f710398aa6c8e5babb965a6989476e5a28cad
Frédéric Devernay <frederic.devernay@m4x.org>
2018-01-23 14:02:45 +0100

    fix python test

--- TestPY/test___01.py
+++ TestPY/test___01.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import NatronEngine
 
-f1 = open('test___01-output.txt','w+')
+f1 = open('test___01-output.txt', 'w+')
 
 reader = app.createNode("fr.inria.openfx.ReadOIIO")
 writer = app.createNode("fr.inria.openfx.WriteOIIO")
@@ -15,21 +15,21 @@ print(blur.getScriptName(), file=f1)
 reader.filename.set("input1.png")
 writer.filename.set("output#.jpg")
 
-writer.connectInput(0,blur)
+writer.connectInput(0, blur)
 writer.formatType.set(0)
 writer.frameRange.set(2)
 writer.firstFrame.set(1)
 writer.lastFrame.set(1)
 writer.quality.set(10)
 
-if blur.canConnectInput(0,reader):
-        print("can connect", file=f1)
+if blur.canConnectInput(0, reader):
+    print("can connect", file=f1)
 
-blur.connectInput(0,reader)
-blur.size.setValueAtTime(0,1)
-blur.size.setValueAtTime(10,2)
-blur.size.setValueAtTime(20,3)
-blur.size.set(30,30,4)
+blur.connectInput(0, reader)
+blur.size.setValueAtTime(0, 1)
+blur.size.setValueAtTime(10, 2)
+blur.size.setValueAtTime(20, 3)
+blur.size.set(30, 30, 4)
 
 print(blur.size.getDefaultValue(), file=f1)
 print(blur.size.get().x, file=f1)
@@ -51,21 +51,21 @@ print(blur.size.getValueAtTime(5), file=f1)
 
 if (NatronEngine.natron.getNatronVersionMajor() == 2 and NatronEngine.natron.getNatronVersionMinor() >= 3) or NatronEngine.natron.getNatronVersionMajor() >= 3:
     availLay = blur.getAvailableLayers(-1)
-else
+else:
     availLay = blur.getAvailableLayers()
 
 
 if len(availLay) > 0:
-        print ("getAvailableLayers", file=f1)
+    print ("getAvailableLayers", file=f1)
 
-if blur.addUserPlane("MyLayer",["R", "G", "B", "A"]):
-        print("added user plane", file=f1)
+if blur.addUserPlane("MyLayer", ["R", "G", "B", "A"]):
+    print("added user plane", file=f1)
 
 print(str(blur.getBitDepth()), file=f1)
 
-getCol=str(blur.getColor())
+getCol = str(blur.getColor())
 if getCol:
-        print("getColor", file=f1)
+    print("getColor", file=f1)
 
 print(str(blur.getCurrentTime()), file=f1)
 print(str(blur.getFrameRate()), file=f1)
@@ -86,4 +86,3 @@ print(str(sizeParam.getIsVisible()), file=f1)
 print(str(sizeParam.getNumDimensions()), file=f1)
 print(str(sizeParam.getScriptName()), file=f1)
 print(str(sizeParam.getTypeName()), file=f1)
-
--- TestPY/test___02.py
+++ TestPY/test___02.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import NatronEngine
 
-f1 = open('test___02-output.txt','w+')
-f2 = open('test___02-output-tmp.txt','w+')
+f1 = open('test___02-output.txt', 'w+')
+f2 = open('test___02-output-tmp.txt', 'w+')
 
 reader = app.createNode("fr.inria.openfx.ReadOIIO")
 writer = app.createNode("fr.inria.openfx.WriteOIIO")
@@ -12,7 +12,7 @@ writer = app.Write1
 reader.filename.set("input1.png")
 writer.filename.set("output#.jpg")
 
-writer.connectInput(0,reader)
+writer.connectInput(0, reader)
 writer.formatType.set(0)
 writer.frameRange.set(2)
 writer.firstFrame.set(1)
@@ -39,6 +39,5 @@ print(str(NatronEngine.natron.isWindows), file=f2)
 
 f2.close()
 with open('test___02-output-tmp.txt') as f:
-	numsum=sum(1 for _ in f)
+    numsum = sum(1 for _ in f)
 print(numsum, file=f1)
-
